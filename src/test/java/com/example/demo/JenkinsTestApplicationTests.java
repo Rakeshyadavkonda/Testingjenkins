997817fb4e860b4e7c476c749084ff8f47337fa6
Rakeshyadav <kondarakeshyadav26@gmail.com>
2023-08-14 11:33:18 +0530

    added first code in the repo

--- src/test/java/com/example/demo/JenkinsTestApplicationTests.java
+++ src/test/java/com/example/demo/JenkinsTestApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsTestApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(JenkinsTestApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("test cases executing.....");
+		assertEquals(true, true);
 	}
 
 }
